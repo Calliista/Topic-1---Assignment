4b228fffb93bb2b3592b33fba1f5d6235e360e2c
Callista Christer <362686198@GoTVDSB.ca>
2026-09-10 13:04:26 -0400

    Done

--- Topic 1 - Assignment/Program.cs
+++ Topic 1 - Assignment/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Callista.
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("Hello, My name is Callista");
+            Console.WriteLine();
+            Console.WriteLine("Comments are used to explain what parts of the code do and/or for the creator to make notes to themselves.");
+            Console.WriteLine();
+            // My anitials
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" CCC   CCC ");
+            Console.WriteLine("C   C C   C");
+            Console.WriteLine("C     C    ");
+            Console.WriteLine("C     C    ");
+            Console.WriteLine("C     C    ");
+            Console.WriteLine("C   C C   C");
+            Console.WriteLine(" CCC   CCC ");
+            Console.ForegroundColor= ConsoleColor.Cyan;
         }
     }
 }
